3ffa59778193eb9266fac81f55653a8ad65152f6
FlavianDurigu <flavi2k@gmail.com>
2017-03-23 12:47:14 +0200

    tutorial - switch commands

---
+++ src/test/java/tutorial/SwitchCommands.java
@@ -0,0 +1,62 @@
+package tutorial;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by FlavianDurigu on 3/23/2017.
+ */
+public class SwitchCommands {
+
+    private static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:\\\\KIT\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://toolsqa.wpengine.com/automation-practice-switch-windows/");
+
+        //parent window
+        String parentHandle = driver.getWindowHandle();
+        System.out.println("Parent Handle: " +parentHandle);
+
+        //WebElement newMsg = driver.findElement(By.cssSelector("[onclick='newMsgWin()']"));
+        WebElement newMsg = driver.findElement(By.cssSelector("#button1"));
+        newMsg.click();
+
+        Set handles = driver.getWindowHandles();
+        System.out.println("Handles: " +handles);
+
+        //switch to second window (pop-up)
+        for(String handle1:driver.getWindowHandles()) {
+            System.out.println(handle1);
+            driver.switchTo().window(handle1);
+        }
+
+        //close pop up window
+        driver.close();
+
+        //back to first window
+        driver.switchTo().window(parentHandle);
+
+        //click AlertBox
+        WebElement alertBox = driver.findElement(By.id("alert"));
+        alertBox.click();
+
+        Alert myAlert = driver.switchTo().alert();
+        Thread.sleep(1000);
+        myAlert.accept();
+
+        Thread.sleep(1000);
+
+        //close original window
+        driver.quit();
+    }
+}
