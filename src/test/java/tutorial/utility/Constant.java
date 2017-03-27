8df11aa2d0ec4b90d6b8f67913db1f480f81efcc
FlavianDurigu <flavi2k@gmail.com>
2017-03-27 17:05:34 +0300

    excel file added

---
+++ src/test/java/tutorial/utility/Constant.java
@@ -0,0 +1,16 @@
+package tutorial.utility;
+
+/**
+ * Created by FlavianDurigu on 3/27/2017.
+ */
+public class Constant {
+    public static final String workingDir = System.getProperty("user.dir");
+    public static final String URL_store = "http://www.store.demoqa.com";
+    public static final String Username = "clr";
+    public static final String Password = "test123";
+    public static final String Path_TestData = workingDir+"/src/test/java/tutorial/utility";
+    public static final String File_TestData = "TestData.xlsx";
+
+
+
+}
